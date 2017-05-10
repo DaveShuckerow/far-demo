ace5d10596b3b80f8b535a8673265c2bf654f379
Dave Shuckerow <djshuckerow@gmail.com>
2017-05-10 03:41:03 -0400

    Redesign the store responses a bit.

--- service/lib/src/stores/store.dart
+++ service/lib/src/stores/store.dart
@@ -3,11 +3,13 @@ import 'package:fixnum/fixnum.dart';
 /// A simple version of a store.
 ///
 /// Each store retrieves data by a parameter, `P`, and returns it as a data
-/// type `R`.
+/// type `D`.
 ///
 /// The store provides a builtin cache system.
 abstract class Store<P, D> {
   Response<D> get(P param);
+
+  Map<P, Response<D>> _cache = <P, Response<D>>{};
 }
 
 class Response<D> {
@@ -15,6 +17,10 @@ class Response<D> {
   final Error error;
 
   Response(this.data, this.error);
-}
 
-enum ResponseStatus { loading, complete, error }
+  bool get isLoading => data == null && error == null;
+
+  bool get isLoaded => data != null && error == null;
+
+  bool get isError => error != null;
+}
