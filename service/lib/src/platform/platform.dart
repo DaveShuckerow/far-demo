30b803df410a8f7d2f463fa985c62a872ad68af9
Dave Shuckerow <djshuckerow@gmail.com>
2017-05-10 19:30:08 -0400

    Add a platform-specific interface to the service layer

---
+++ service/lib/src/platform/platform.dart
@@ -0,0 +1,10 @@
+/// Locator for all service functionality from the platform.
+///
+/// Having this interface around allows us to depend on platform-specific
+/// functionality, such as authentication, inside of our service layer while
+/// still not importing either Angular or Flutter dependencies into the shared
+/// code layer.
+///
+/// The Flutter and Angular libraries will be responsible for providing an
+/// implementation of Platform.
+abstract class Platform {}
