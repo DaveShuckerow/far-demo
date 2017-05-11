bec560f028a4d8a53fe493fec0d2bd1e96d4e19e
Dave Shuckerow <djshuckerow@gmail.com>
2017-05-11 00:25:35 -0400

    Add a room reference to the room messages shower.

--- web/lib/app_component.dart
+++ web/lib/app_component.dart
@@ -1,10 +1,12 @@
 import 'package:angular2/core.dart';
 
+import 'package:github.daveshuckerow.chat.web/src/room_messages/room_messages_component.dart';
+
 @Component(
-  selector: 'chat-app',
-  templateUrl: 'app_component.html',
-  styleUrls: const ['app_component.css'],
-)
+    selector: 'chat-app',
+    templateUrl: 'app_component.html',
+    styleUrls: const ['app_component.css'],
+    directives: const [RoomMessagesComponent])
 class AppComponent {
   var name = 'Angular';
 }
--- web/lib/src/room_messages/room_messages_component.dart
+++ web/lib/src/room_messages/room_messages_component.dart
@@ -1,10 +1,13 @@
 import 'package:angular2/core.dart';
+import 'package:fixnum/fixnum.dart';
 
 import 'package:github.daveshuckerow.chat.service/service.dart';
 
-@Component()
+@Component(
+  selector: 'room-messages',
+  template: '<p>Messages from {{currentRoom.name}}</p>',
+)
 class RoomMessagesComponent {
   final RoomRef currentRoom;
-
-  RoomMessagesComponent(this.currentRoom);
+  RoomMessagesComponent() : currentRoom = new RoomRef(Int64.ONE, 'My room');
 }
