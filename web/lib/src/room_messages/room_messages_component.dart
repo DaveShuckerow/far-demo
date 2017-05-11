2a90af422b31f9bc46f68adcb43a3ea2020e4681
Dave Shuckerow <djshuckerow@gmail.com>
2017-05-11 01:39:22 -0400

    Show messages in the UI.

--- web/lib/src/room_messages/room_messages_component.dart
+++ web/lib/src/room_messages/room_messages_component.dart
@@ -1,3 +1,4 @@
+import 'package:angular2/common.dart';
 import 'package:angular2/core.dart';
 import 'package:fixnum/fixnum.dart';
 
@@ -5,9 +6,26 @@ import 'package:github.daveshuckerow.chat.service/service.dart';
 
 @Component(
   selector: 'room-messages',
-  template: '<p>Messages from {{currentRoom.name}}</p>',
+  templateUrl: 'room_messages_component.html',
+  styleUrls: const ['room_messages_component.css'],
+  directives: const [NgFor, NgIf],
 )
 class RoomMessagesComponent {
-  final RoomRef currentRoom;
-  RoomMessagesComponent() : currentRoom = new RoomRef(Int64.ONE, 'My room');
+  static final RoomRef currentRoom = new RoomRef(Int64.ONE, 'My room');
+  static final UserRef me = new UserRef(Int64.ZERO, 'Me');
+  static final UserRef you = new UserRef(Int64.ONE, 'You');
+
+  final List<Message> messages = [
+    new Message(Int64.ZERO, '', 'Hello, what\'s up?', me, currentRoom),
+    new Message(
+        Int64.ONE, '', 'Not much, what`s up wit you?', you, currentRoom),
+    new Message(Int64.TWO, '', 'Oh everything.', me, currentRoom),
+    new Message(Int64.TWO, '', 'We`re writing all this code and stuff', me,
+        currentRoom),
+    new Message(Int64.TWO, '', 'It`s just so busy.', me, currentRoom),
+  ];
+
+  RoomMessagesComponent();
+
+  bool isFromMe(Message message) => message.sender.id == me.id;
 }
