8fdaf9ee3a15b8170a255e7219b8a1ac28e18549
Dave Shuckerow <djshuckerow@gmail.com>
2017-05-12 00:47:08 -0400

    Handle subscriptions to stores at the app level.

--- web/lib/src/state/subscribing_component.dart
+++ web/lib/src/state/subscribing_component.dart
@@ -1,18 +1,30 @@
 import 'package:angular2/core.dart';
 import 'package:github.daveshuckerow.chat.service/service.dart';
 
-class SubscribingComponent implements OnInit, OnDestroy {
+/// Handler for subscriptions to the app's state.
+///
+/// Manages subscriptions to [Store]s and calls `refresh` when a store's data
+/// updates
+///
+/// Unsubscribes when the component is destroyed.
+abstract class SubscribingComponent implements OnInit, OnDestroy {
   final List<Store> subscriptions;
 
   SubscribingComponent(this.subscriptions);
 
+  void refresh();
+
   @override
   void ngOnInit() {
-    for (var store in subscriptions) {}
+    for (var store in subscriptions) {
+      store.subscribe(this, refresh);
+    }
   }
 
   @override
   void ngOnDestroy() {
-    for (var store in subscriptions) {}
+    for (var store in subscriptions) {
+      store.unsubscribe(this);
+    }
   }
 }
