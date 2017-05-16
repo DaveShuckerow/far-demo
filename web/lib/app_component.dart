cb8ac7f2d8a76773de789ccfee39e59d107feb1a
Dave Shuckerow <djshuckerow@gmail.com>
2017-05-15 22:05:00 -0400

    Add a message input component.

--- web/lib/app_component.dart
+++ web/lib/app_component.dart
@@ -1,12 +1,12 @@
 import 'package:angular2/core.dart';
 
+import 'package:github.daveshuckerow.chat.web/src/message_input/message_input.dart';
 import 'package:github.daveshuckerow.chat.web/src/room_messages/room_messages_component.dart';
 
 @Component(
-    selector: 'chat-app',
-    templateUrl: 'app_component.html',
-    styleUrls: const ['app_component.css'],
-    directives: const [RoomMessagesComponent])
-class AppComponent {
-  var name = 'Angular';
-}
+  selector: 'chat-app',
+  templateUrl: 'app_component.html',
+  styleUrls: const ['app_component.css'],
+  directives: const [RoomMessagesComponent, MessageInputComponent],
+)
+class AppComponent {}
---
+++ web/lib/src/message_input/message_input.dart
@@ -0,0 +1,11 @@
+import 'package:angular2/core.dart';
+
+@Component(
+  selector: 'message-input',
+  templateUrl: 'message_input.html',
+  styleUrls: const ['message_input.css'],
+)
+class MessageInputComponent {
+  @override
+  void refresh() {}
+}
