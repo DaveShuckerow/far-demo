f8dc309418d946d4ad9f4310cf89e8d1bc1847bc
Dave Shuckerow <djshuckerow@gmail.com>
2017-05-16 00:27:48 -0400

    Simplify providers.

--- web/web/main.dart
+++ web/web/main.dart
@@ -1,6 +1,7 @@
 import 'package:angular2/platform/browser.dart';
 
 import 'package:github.daveshuckerow.chat.web/app_component.dart';
+import 'package:github.daveshuckerow.chat.web/src/module/module.dart';
 
 main() {
   print('starting the app up.');
