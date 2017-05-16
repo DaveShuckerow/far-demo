53323dbb16553283d481f95a70b4a0c5e856f7fe
Dave Shuckerow <djshuckerow@gmail.com>
2017-05-16 00:27:31 -0400

    Simplify providers.

--- web/lib/src/module/module.dart
+++ web/lib/src/module/module.dart
@@ -2,8 +2,8 @@ import 'package:angular2/di.dart';
 import 'package:github.daveshuckerow.chat.service/service.dart';
 
 const bindings = const [
-  const Provider(MessageStore, useClass: MessageStore),
-  const Provider(UserStore, useClass: UserStore),
-  const Provider(RoomStore, useClass: RoomStore),
-  const Provider(MessageMutator, useClass: MessageMutator),
+  MessageStore,
+  UserStore,
+  RoomStore,
+  MessageMutator,
 ];
--- web/web/main.dart
+++ web/web/main.dart
@@ -1,12 +1,8 @@
 import 'package:angular2/platform/browser.dart';
 
 import 'package:github.daveshuckerow.chat.web/app_component.dart';
-import 'package:github.daveshuckerow.chat.web/src/module/module.dart';
 
 main() {
   print('starting the app up.');
-  bootstrap(
-    AppComponent,
-    bindings,
-  );
+  bootstrap(AppComponent, providers: bindings);
 }
