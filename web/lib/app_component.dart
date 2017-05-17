42d23605e0520e9cc714f751c404195fc93006da
Dave Shuckerow <djshuckerow@gmail.com>
2017-05-16 22:17:07 -0400

    Room list start work.

--- web/lib/app_component.dart
+++ web/lib/app_component.dart
@@ -1,12 +1,17 @@
 import 'package:angular2/core.dart';
 
 import 'package:github.daveshuckerow.chat.web/src/message_input/message_input.dart';
+import 'package:github.daveshuckerow.chat.web/src/room_list/room_list.dart';
 import 'package:github.daveshuckerow.chat.web/src/room_messages/room_messages_component.dart';
 
 @Component(
   selector: 'chat-app',
   templateUrl: 'app_component.html',
   styleUrls: const ['app_component.css'],
-  directives: const [RoomMessagesComponent, MessageInputComponent],
+  directives: const [
+    RoomMessagesComponent,
+    MessageInputComponent,
+    RoomListComponent,
+  ],
 )
 class AppComponent {}
---
+++ web/lib/src/room_list/room_list.dart
@@ -0,0 +1,22 @@
+import 'package:angular2/common.dart';
+import 'package:angular2/core.dart';
+import 'package:github.daveshuckerow.chat.service/service.dart';
+import 'package:github.daveshuckerow.chat.web/src/module/platform.dart';
+
+@Component(
+  selector: 'room-list',
+  templateUrl: 'room_list.html',
+  styleUrls: const ['room_list.css'],
+  providers: const [NgFor],
+)
+class RoomListComponent {
+  final User currentUser;
+
+  RoomListComponent(FakePlatformImpl platform)
+      : currentUser = platform.currentUser {
+    print(platform != null);
+    print('User: ${currentUser != null}');
+  }
+
+  List<RoomRef> get roomRefs => currentUser?.rooms;
+}
