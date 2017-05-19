c007dcb58168a111e6dd3acb21da7f848f306bb0
Dave Shuckerow <djshuckerow@gmail.com>
2017-05-19 18:57:25 -0400

    Move things around a little.

---
+++ web/lib/src/components/app_component/app.dart
@@ -0,0 +1,23 @@
+import 'package:angular2/core.dart';
+
+import 'package:angular2/router.dart';
+import 'package:github.daveshuckerow.chat.web/src/components/message_input/message_input.dart';
+import 'package:github.daveshuckerow.chat.web/src/components/room_messages/room_messages.dart';
+
+@Component(
+  selector: 'chat-app',
+  templateUrl: 'app_component.html',
+  styleUrls: const ['app_component.css'],
+  directives: const [
+    MessageInputComponent,
+    RoomMessagesComponent,
+    ROUTER_DIRECTIVES,
+  ],
+)
+@RouteConfig(const [
+  const Route(
+    path: '/',
+    name: 'Chat',
+  )
+])
+class AppComponent {}
--- web/lib/src/components/room_messages/room_messages.dart
+++ web/lib/src/components/room_messages/room_messages.dart
@@ -1,5 +1,6 @@
 import 'package:angular2/common.dart';
 import 'package:angular2/core.dart';
+import 'package:angular2/router.dart';
 import 'package:fixnum/fixnum.dart';
 import 'package:github.daveshuckerow.chat.service/service.dart';
 import 'package:github.daveshuckerow.chat.web/src/state/subscribing_component.dart';
@@ -15,34 +16,27 @@ class RoomMessagesComponent extends SubscribingComponent
   final RoomStore roomStore;
   final MessageStore messageStore;
   final User currentUser;
-
-  @Input()
-  set roomRef(RoomRef ref) {
-    _roomRef = ref;
-    print('setting room ref to $ref');
-    refresh();
-  }
-
-  RoomRef get roomRef => _roomRef;
-
-  RoomRef _roomRef;
+  final RouteParams routeParams;
 
   List<Message> messages;
 
-  RoomMessagesComponent(this.roomStore, this.messageStore, this.currentUser)
+  RoomMessagesComponent(
+      this.roomStore, this.messageStore, this.currentUser, this.routeParams)
       : super([roomStore, messageStore]);
 
   bool isFromMe(Message message) => message?.sender == currentUser;
 
+  int get _roomId => int.parse(routeParams.get('roomId'), onError: (_) => null);
+
   @override
   void refresh() {
-    if (roomRef == null) return;
-    print('loading data for room ${roomRef.id}');
-    var room = roomStore.get(roomRef);
+    if (_roomId == null) return;
+    print('loading data for room $_roomId');
+    var room = roomStore.get(new RoomRef(new Int64(_roomId), ''));
     if (room == null) return;
     messages = <Message>[];
     for (var i = 0; i < room.messageCount.toInt(); i++) {
-      var message = messageStore.get(new MessageRef(new Int64(i), roomRef));
+      var message = messageStore.get(new MessageRef(new Int64(i), room));
       messages.add(message);
     }
   }
