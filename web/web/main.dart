cf75d7d057266c55d03cbcce189dfe4cb549211a
Dave Shuckerow <djshuckerow@gmail.com>
2017-05-19 18:58:47 -0400

    Finish moving around

--- web/web/main.dart
+++ web/web/main.dart
@@ -1,6 +1,6 @@
 import 'package:angular2/platform/browser.dart';
 
-import 'package:github.daveshuckerow.chat.web/app_component.dart';
+import 'package:github.daveshuckerow.chat.web/src/components/app/app.dart';
 import 'package:github.daveshuckerow.chat.web/src/module/module.dart';
 
 main() {
