9f8ab177113f4fff600ed87786f7ab92b32e00df
Dave Shuckerow <djshuckerow@gmail.com>
2017-05-19 22:21:20 -0400

    Add a real room store.

--- service/lib/src/state/room_store.dart
+++ service/lib/src/state/room_store.dart
@@ -1,14 +1,36 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:angular2/di.dart';
 
 import '../entities/room.dart';
 import '../fake/db.dart';
 import 'message_mutator.dart';
+import 'package:github.daveshuckerow.chat.service/service.dart';
 import 'store.dart';
 
 @Injectable()
 class RoomStore extends Store<RoomRef, Room> {
-  RoomStore(MessageMutator messageMutator) {
+  final Platform _platform;
+  RoomStore(MessageMutator messageMutator, this._platform) {
+    messageMutator.subscribe(this, clearCache);
+  }
+
+  @override
+  Future<Room> load(RoomRef param) async {
+    var response = await _platform.get('rooms/${param.id}');
+    if (response.statusCode == 200) {
+      var json = JSON.decode(response.body);
+      return new Room.fromJson(json);
+    } else {
+      print('Error: ${response.body}');
+      return null;
+    }
+  }
+}
+
+@Injectable()
+class RoomStoreFake extends Store<RoomRef, Room> implements RoomStore {
+  RoomStoreFake(MessageMutator messageMutator) {
     messageMutator.subscribe(this, clearCache);
   }
 
@@ -17,4 +39,7 @@ class RoomStore extends Store<RoomRef, Room> {
     await new Future.delayed(const Duration(seconds: 1));
     return rooms[param.id];
   }
+
+  @override
+  Platform get _platform => null;
 }
